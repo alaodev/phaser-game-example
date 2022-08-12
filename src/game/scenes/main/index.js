a975dfe7e513b82c8f9f601d11d43ac4bc1be3b9
alaodev <alaodev@gmail.com>
2022-08-12 12:03:18 -0300

    add: loading assets

--- src/game/scenes/main/index.js
+++ src/game/scenes/main/index.js
@@ -8,6 +8,19 @@ class MainScene extends Phaser.Scene {
   init() {
     this.cameras.main.setBackgroundColor('#000000')
   }
+
+  preload() {
+    this.load.image('sky', 'assets/images/sky.png')
+    this.load.image('bomb', 'assets/images/bomb.png')
+    this.load.image('ground', 'assets/images/platform.png')
+    this.load.image('star', 'assets/images/star.png')
+
+    this.load.spritesheet('dude',
+      'assets/sprites/dude.png',
+      { frameWidth: 32, frameHeight: 48 }
+    )
+  }
+
 }
 
 export default MainScene
